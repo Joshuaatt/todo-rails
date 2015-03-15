f5746d46ce8c9b5c4046f1975a06462e3c1cba4b
Joshua Atteberry <joshuaatteberry@gmail.com>
2015-03-14 18:59:08 -0700

    clean up controller

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,24 +1,26 @@
 class ListsController < ApplicationController
   def index
     @lists = List.all
-    render :index
   end
+
   def new
     @list = List.new
-    render :new
   end
+
   def create
     @list = List.new(params[:list])
     if @list.save
+      flash[:notice] = "List successfully added!"
       redirect_to lists_path
     else
       render :new
     end
   end
+
   def edit
     @list = List.find(params[:id])
-    render :edit
   end
+
   def update
     @list = List.find(params[:id])
     if @list.update(params[:list])
@@ -27,8 +29,14 @@ class ListsController < ApplicationController
       render :edit
     end
   end
+
+  def destroy
+    @list = List.find(params[:id])
+    @list.destroy
+    redirect_to lists_push
+  end
+
   def show
     @list = List.find(params[:id])
-    render :show
   end
 end
